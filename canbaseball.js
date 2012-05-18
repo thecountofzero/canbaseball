9fc49c10785ee00dc14126a708aa992ba3fb187e
Mike Malamud <mike.malamud@gmail.com>
2012-05-18 10:15:37 -0400

    updates to cause error

--- canbaseball.js
+++ canbaseball.js
@@ -1,16 +1,25 @@
 steal({src: 'canbaseball/fixtures/fixtures.js', ignore: true});
 
+
 steal('can/control', 'can/view/ejs').
-	then('canbaseball/widgets/a', 'canbaseball/widgets/b', function($) {
+	then('canbaseball/lib/utils/utils_alt.js').
+	then('./canbaseball.css').
+	then('canbaseball/widgets/a', 'canbaseball/widgets/b', 'canbaseball/widgets/c', function($) {
 
 	canbaseball = can.Control({
-		// Initialize the Dashboard
 		init : function() {
 		}
 	});
 
 	// Initialize the app
 	//new CloudArray.StatusPanel('#statusPanel', {});
-	new A('#scoreboard', {});
-	new B('#cacheOverview', {});
+	new A('#a', {});
+	new B('#b', {});
+	new C('#c', {});
+
+	console.log('is object: ' + MYAPP.utils.isObject(MYAPP.utils));
+	console.log('something: ' + $MYAPP.utils.something());
+
+	//var a = can.getObject('MYAPP.local',[], true);
+	console.dir(MYAPP);
 });
