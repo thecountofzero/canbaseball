56a7c8fdc11b357a903b22676c840500da77665d
Mike Malamud <mike.malamud@gmail.com>
2012-06-16 21:18:11 -0400

    Updated to use app switcher

--- canbaseball.js
+++ canbaseball.js
@@ -5,37 +5,19 @@ then('canbaseball/lib/utils',
     'canbaseball/lib/ajaxsettings.js',
     'canbaseball/widgets/status_panel',
     'canbaseball/widgets/navigation',
+    'canbaseball/widgets/app_switcher',
     'canbaseball/dashboard',
     'canbaseball/resources/css', function($) {
-
-    Router = can.Control({
-
-        init: function() {
-            this.currentApp = '';
-            this.mainContent = $('#mainContent');
-        },
-
-        "{can.route} app": function(data) {
-            var app = data.app;
-
-            if(this.currentApp !== app) {
-                this.mainContent.empty().append('<div id="app"></div>');
-
-                if(app === 'dashboard') {
-                    console.log('loading dashboard');
-                    new CanBaseball.Dashboard(this.mainContent.find('#app'));
-                }
-            }
-
-            this.currentApp = app;
-        }
-    });
 
     can.route(':app');
     can.route(':app/:appId');
     can.route('', {app: 'dashboard'});
 
-    new Router(window);
+    new CanBaseball.AppSwitcher($('#mainContent'), {
+        apps: {
+            'dashboard': CanBaseball.Dashboard
+        }
+    });
 
     $(document).ready(function() {
         new CanBaseball.Navigation('nav', {});
