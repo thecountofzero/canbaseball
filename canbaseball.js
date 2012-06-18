5a0cc9ff80f4aab88bd89b19f3b277dd28dc9eaf
Mike Malamud <mike.malamud@gmail.com>
2012-06-18 15:47:27 -0400

    Implemented ejs views for most of main content

--- canbaseball.js
+++ canbaseball.js
@@ -1,26 +1,39 @@
 steal({src: 'canbaseball/fixtures/fixtures.js', ignore: true});
 
 steal('can/control', 'can/view/ejs', 'can/route', 'can/control/route').
-then('canbaseball/lib/utils',
+then('canbaseball/lib/canbaseball/utils',
+    'canbaseball/plugins/localization.js',
     'canbaseball/lib/ajaxsettings.js',
     'canbaseball/widgets/status_panel',
     'canbaseball/widgets/navigation',
-    'tcoz/app_switcher',
+    'canbaseball/tcoz/app_switcher',
     'canbaseball/dashboard',
     'canbaseball/widgets/standings',
     'canbaseball/resources/css', function($) {
 
+    can.route(':app');
+    can.route(':app/:appId');
+    can.route('', {app: 'dashboard'});
 
-    new TCOZ.AppSwitcher($('#mainContent'), {
-        apps: {
-            'dashboard': CanBaseball.Dashboard,
-            'standings': CanBaseball.Standings
-        }
-    });
+    $tcoz.localization.init().loadDictionary('resources/locales', function() {
 
-    $(document).ready(function() {
-        new CanBaseball.Navigation('nav', {});
-        new CanBaseball.StatusPanel('.topContent', {});
+        $(document).ready(function() {
+
+            new TCOZ.AppSwitcher($('#mainContent'), {
+                apps: {
+                    'dashboard': CanBaseball.Dashboard,
+                    'standings': CanBaseball.Standings
+                },
+                useAppSpace: false
+            });
+
+            $('.topNav').append(can.view('views/topnav.ejs'));
+            $('.search').append(can.view('views/search.ejs'));
+            $('footer').append(can.view('views/footer.ejs'));
+
+            new CanBaseball.Navigation('nav', {});
+            new CanBaseball.StatusPanel('.topContent', {});
+        });
     });
     
 });
