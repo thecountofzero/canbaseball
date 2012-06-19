1b218831da68cee50cf9512c1b324d88b9ff54d0
Mike Malamud <mike.malamud@gmail.com>
2012-06-19 11:41:00 -0400

    Moved app into canbaseball directory

--- plugins/qtip2/qtip2.js
+++
@@ -1,9 +0,0 @@
-steal('./jquery.qtip.js', './jquery.qtip.css', function() {
-
-	// Set the defaults for the qTip tooltip plugin
-	$.extend(true, $.fn.qtip.defaults, {
-		style: {
-			classes: "ui-tooltip-blue ui-tooltip-rounded ui-tooltip-shadow"
-		}
-	});
-});
--- resources/css/css.js
+++
@@ -1,3 +0,0 @@
-steal('./reset.css')
-.then('./common.css')
-.then('./ui_custom.css', './icons.css', './jquery.qtip.css');
