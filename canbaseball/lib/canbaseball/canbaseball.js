d2187260c7011aaa510f1acec34c68dd5311bdc0
Mike Malamud <mike.malamud@gmail.com>
2012-06-21 00:05:48 -0400

    Restructured

--- canbaseball/lib/canbaseball/canbaseball.js
+++ canbaseball/lib/canbaseball/canbaseball.js
@@ -1,71 +1,52 @@
 steal('can/util/jquery', function($) {
 
-	(function(window) {
-		
-		var CANBASEBALL = window.CANBASEBALL || {};
+    CANBASEBALL = window.CANBASEBALL || {
+        initialized: false,
+        config: {},
+        about: {}
+    };
 
-		init = function() {
+    var init = function() {
 
-			$.when(CANBASEBALL.getConfig()).then(function(config) {
-				
-				config = config.config;
-				CANBASEBALL.config = config;
+        $.when(CANBASEBALL.getConfig()).then(function(config) {
+            
+            config = config.config;
+            CANBASEBALL.config = config;
 
-				CANBASEBALL.about = (function() {
-					return {
-						"version": config.javascriptAPI.version,
-						"name": config.javascriptAPI.name
-					};
-				})();
-			});
-		};
+            CANBASEBALL.about =  {
+                "version": config.javascriptAPI.version,
+                "name": config.javascriptAPI.name
+            };
+        });
+    };
 
-		CANBASEBALL.getConfig = function(force) {
+    can.extend(CANBASEBALL, {
 
-			var config = {};
-			force = force || false;
+        getConfig: function(force) {
 
-			if(!CANBASEBALL.initialized || force) {
-				return $.ajax({
-					url: 'resources/config/config.json',
-					type: 'GET',
-					dataType: 'json',
-					async: false,
-					success: function(data, textStatus, jqXHR) {
-						config = data;
-						CANBASEBALL.initialized = true;
-					}
-				});
-			}
-			else {
-				return config;
-			}
-		};
-		
-		CANBASEBALL.namespace = function(ns) {
-			var parts = ns.split('.'),
-				parent = CANBASEBALL,
-				i;
-			
-			if(parts[0] === "CANBASEBALL") {
-				parts = parts.slice(1);
-			}
-			
-			for(i=0; i<parts.length; i+=1) {
-				if(typeof parent[parts[i]] === "undefined") {
-					parent[parts[i]] = {};
-				}
-				parent = parent[parts[i]];
-			}
-			return parent;
-		};
+            var config = {};
+            force = force || false;
 
-		if(!CANBASEBALL.initialized) {
-			init();
-		}
-		
-		window.CANBASEBALL = $CANBASEBALL = CANBASEBALL;
-		
-	}(window));
-	
+            if(!this.initialized || force) {
+
+                return $.ajax({
+                    url: 'resources/config/config.json',
+                    type: 'GET',
+                    dataType: 'json',
+                    async: false,
+                    success: function(data, textStatus, jqXHR) {
+                        config = data;
+                        CANBASEBALL.initialized = true;
+                    }
+                });
+            }
+            else {
+                return config;
+            }
+        }
+    });
+
+    if(!CANBASEBALL.initialized) {
+        init();
+    }
 });
