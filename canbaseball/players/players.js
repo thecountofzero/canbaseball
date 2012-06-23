8c6252895a930483f6235940bf4a82fbb1ecfa2b
Mike Malamud <mike.malamud@gmail.com>
2012-06-22 23:34:22 -0400

    Fix namespacing

--- canbaseball/players/players.js
+++ canbaseball/players/players.js
@@ -1,34 +1,12 @@
-steal({src: 'canbaseball/fixtures/fixtures.js', ignore: true});
+steal('can/control', function($) {
 
-steal('can/control', 'can/view/ejs', 'can/route', 'can/control/route').
-then('canbaseball/widgets/status_panel',
-    'canbaseball/widgets/navigation',
-    'canbaseball/widgets/standings',
-    'canbaseball/widgets/stats_leaders',
-    'canbaseball/ui/collapsible', function($) {
+    window.CanBaseball = window.CanBaseball || {};
+    window.CanBaseball.Apps = window.CanBaseball.Apps || {};
 
-    can.Control("CanBaseball.Players", {
-        // Initialize the Dashboard
+    window.CanBaseball.Apps.Players = can.Control({
+
         init : function() {
-            var self = this;
-
-            this.element.append(can.view('//canbaseball/dashboard/dashboard.ejs', {}));
-
-            new CanBaseball.Standings('#standingsContent', {});
-            new CanBaseball.StatsLeaders('#statsContent', {});
-
-            new CanBaseball.UI.Collapsible('#standings', {});
-            new CanBaseball.UI.Collapsible('#stats', {});
-            //new CanBaseball.UI.Collapsible('#playerNews', {});
-        },
-
-        "{can.route} appId": function(route, ev, newVal, oldVal) {
-            console.log('change: ' + newVal + ':' + oldVal);
-        },
-
-        destroy: function() {
-            console.log('dashboard destroy');
-            can.Control.prototype.destroy.call(this);
+            this.element.append('Player App');
         }
     });
 });
